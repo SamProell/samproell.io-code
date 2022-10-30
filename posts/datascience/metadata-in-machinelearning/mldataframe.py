8914626b44626ad0e1345aeb9587e2c84f6def47
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2022-10-30 12:11:22 +0100

    add docstrings to MLDataFrame

--- posts/datascience/metadata-in-machinelearning/mldataframe.py
+++ posts/datascience/metadata-in-machinelearning/mldataframe.py
@@ -1,6 +1,20 @@
+"""Provides MLDataFrame class holding machine learning data with metadata."""
 import pandas as pd
 
 class MLDataFrame(pd.DataFrame):
+    """Data frame holding multidimensional data with corresponding metadata.
+
+    Args:
+        data: any data input acceptable by pd.DataFrame
+        x_data (np.ndarray): input data array, samples indexed along first dim.
+        y_data (np.ndarray): target data array, indexed along first dimension.
+        kwargs: additional keyword arguments accepted by pd.DataFrame.
+
+    Attributes:
+        x_data: machine learning inputs.
+        y_data: machine learning targets.
+
+    """
     _metadata = ["_x_data", "_y_data"]
 
     @property
@@ -17,13 +31,14 @@ class MLDataFrame(pd.DataFrame):
 
     @property
     def x_data(self):
+        """Machine learning inputs."""
         if self._x_data is None:
             raise ValueError("Cannot access unspecified x_data")
         return self._x_data[self.index]
 
     @property
     def y_data(self):
+        """Machine Learning targets."""
         if self._y_data is None:
             raise ValueError("Cannot access unspecified y_data")
         return self._y_data[self.index]
-
